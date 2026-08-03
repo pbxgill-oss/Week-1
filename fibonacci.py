54ae1fcbb49e118db74ce9b7efb192c006b7db1e
Sukhjeet Singh <pbxgill@gmail.com>
2026-08-03 17:01:20 +1200

    Add comments in fibonacci series
    Added comments to explain the purpose of the fibonacci and factorial functions.

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
+    # Function to print fibonacci series
+def fibonacci(n): 
     a = 0
     b = 1
     print ("fibonacci series:")
@@ -9,14 +10,16 @@ def fibonacci(n):
         b = c
     print()    
 
+     # function to print factorial of last number
 def factorial(n):
     fact = 1
     for i in range(1, n + 1):
         fact *= i
     print("factorial of ", n, "is:", fact)
-
+    
+     # Main function
 def main():
-    n = int(input("enter any number :"))
+    n = int(input("enter any number :"))   # taking number from user
     fibonacci(n)
     factorial(n)
 
